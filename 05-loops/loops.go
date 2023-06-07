8eb82dd25e498e9b8bd5b868b67d3d13b885f435
LinC3e <ivancostilla17@gmail.com>
2023-06-07 18:07:51 -0300

    loops

---
+++ 05-loops/loops.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	fmt.Println("------------------------------")
+
+	i := 1
+	for i < 10 {
+		fmt.Println(i)
+		i++
+	}
+}
