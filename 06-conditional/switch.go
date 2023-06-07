502cc5ce865313b7283da4213c0d3bec71550bac
LinC3e <ivancostilla17@gmail.com>
2023-06-07 18:29:24 -0300

    if & switch statements

---
+++ 06-conditional/switch.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	number := 3
+
+	if number > 0 {
+		switch number {
+		case 1:
+			fmt.Println("One")
+		case 2:
+			fmt.Println("Two")
+		case 3:
+			fmt.Println("Three")
+		default:
+			fmt.Println("Its more than Three.")
+		}
+	} else {
+		fmt.Println("Its zero or negative")
+	}
+
+}
